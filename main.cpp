e9f53928c225171a8286e233e2f170d91aeadae0
lucafeldk <157419957+lucafeldk@users.noreply.github.com>
2025-01-22 18:13:57 +0100

    api call to entsoe successful

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include <cpr/cpr.h>
 #include <iostream>
 
-int main(int argc, char** argv) {
-    cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
-                           cpr::Header{{"User-Agent", "MyApp"}});
-    std::cout << r.status_code << std::endl;                  // 200
-    r.header["content-type"];       // application/json; charset=utf-8
-    std::cout << r.text;                         // JSON text string
+int main() {
+    // set url parameters
+    std::string api_key = "0e2c5458-3e40-4c3a-907d-c344020ebad6";
+    std::string base_url = "https://web-api.tp.entsoe.eu/api";
+    std::string document_type = "A75";
+    std::string process_type = "A16";
+    std::string in_domain = "10Y1001A1001A83F"; //countrycode
+    std::string period_start = "202308152200"; // starttime
+    std::string period_end = "202308162200";   // endtime
+
+    //configure request url
+    std::string url = base_url + "?securityToken=" + api_key +
+                      "&documentType=" + document_type +
+                      "&processType=" + process_type +
+                      "&in_Domain=" + in_domain +
+                      "&periodStart=" + period_start +
+                      "&periodEnd=" + period_end;
+
+    //send get request to api
+    cpr::Response response = cpr::Get(cpr::Url{url});
+
+    // error handling
+    if (response.error){
+        std::cerr << "Error:" << response.error.message << std::endl;
+        return 1;
+    }
+
+    // print response
+    std::cout << "Status Code: " << response.status_code << std::endl;
+    std::cout << "Antwort: " << response.text << std::endl;
+    
     return 0;
 }
